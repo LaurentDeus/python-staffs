4b34ecd11b6c21a91ff440ef979106a2b212b75d
LaurentDeus <laurentdeusdedith75@gmail.com>
2024-05-02 17:09:01 +0300

    created user validation function

--- cruds.py
+++ cruds.py
@@ -1,9 +1,19 @@
+from typing import Union
 import models
 from sqlalchemy.orm import Session
 import bcrypt
 
 
-def _add_user_credential(email: str, password: str,dbsession:Session) -> models.Credential:
+def is_valid_user(email: str, password: str) -> Union[models.Booker, None]:
+    with Session(models.engine) as dbsession:
+        booker = dbsession.query(
+            models.Credential).filter_by(email=email).one()
+        if booker and bcrypt.checkpw(password.encode(), booker.password_hash.encode()):
+            return dbsession.query(models.Booker).filter_by(credential=email).one()
+        return None
+
+
+def _add_user_credential(email: str, password: str, dbsession: Session) -> models.Credential:
     password_hash = bcrypt.hashpw(
         password.encode(), bcrypt.gensalt()).decode()
     credential = models.Credential(
@@ -13,9 +23,10 @@ def _add_user_credential(email: str, password: str,dbsession:Session) -> models.
     return credential
 
 
-def create_user(firstname:str, lastname:str, phone:str, email:str, password:str, role='Booker')->models.Booker:
+def create_user(firstname: str, lastname: str, phone: str, email: str, password: str, role='Booker') -> models.Booker:
     with Session(models.engine) as dbsession:
-        user_credential = _add_user_credential(email=email, password=password,dbsession = dbsession)
+        user_credential = _add_user_credential(
+            email=email, password=password, dbsession=dbsession)
         user = models.Booker(firstname=firstname, lastname=lastname,
                              phone=phone, credential=user_credential.email, role=role)
         dbsession.add(user)
